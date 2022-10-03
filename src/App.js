61eaae7e838acf5848e9388db68a726fcd440a50
Sangha Yoon <yoons4@spu.edu>
2022-10-03 12:10:53 +0900

    Sangha Yoon

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import TodoList from './TodoList';
+function App() {
+    const [todos, setTodos] = useState(['Todo 1', 'Todo 2'])
+    return(
+        <>
+        <TodoList todos = {todos} />
+        <input type = "text" />
+        <button>Add Todo</button>
+        <button>Clear Completed</button>
+        <div>0 left to do.</div>
+        </>
+    );
+}
+
+export default App;
---
+++ src/TodoList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TodoList({todos}) {
+  return (
+    <div>
+      Helloooooo World!!!!!
+    </div>
+  )
+}
